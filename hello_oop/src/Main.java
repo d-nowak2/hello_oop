4b64370045f68aaf104e3f0c2d1f8165eb0d6add
David Nowak <nowak.david@gmail.com>
2025-09-05 10:24:56 -0500

    Add files via upload

---
+++ hello_oop/src/Main.java
@@ -0,0 +1,9 @@
+package hello_oop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Greeter g = new Greeter("David Nowak", "Ferrets Should Rule the World!!*!");
+        System.out.println(g.format());
+	}
+}
