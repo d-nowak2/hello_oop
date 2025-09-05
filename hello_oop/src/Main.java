2faa1d68bc00dcc93e4b460df2cd632873cb59ec
David Nowak <136285344+d-nowak2@users.noreply.github.com>
2025-09-05 18:37:51 -0500

    Main.java was located in the wrong folder.

--- hello_oop/src/Main.java
+++
@@ -1,9 +0,0 @@
-package hello_oop;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Greeter g = new Greeter("David Nowak", "Ferrets Should Rule the World!!*!!");
-        System.out.println(g.format());
-	}
-}
