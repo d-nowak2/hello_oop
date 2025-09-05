028860b569f667366911379d5718923da1b0573d
David Nowak <88691461+wedrown@users.noreply.github.com>
2025-09-05 09:10:30 -0500

    Added extra exclamation to the greeting.

--- hello_oop/src/hello_oop/Main.java
+++ hello_oop/src/hello_oop/Main.java
@@ -3,8 +3,7 @@ package hello_oop;
 public class Main {
 
 	public static void main(String[] args) {
-		Greeter g = new Greeter("David Nowak", "Ferrets Should Rule the World!");
+		Greeter g = new Greeter("David Nowak", "Ferrets Should Rule the World!!");
         System.out.println(g.format());
 	}
-
 }
