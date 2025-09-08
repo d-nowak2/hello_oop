74eddcb76a3d4c98f3b24c61edd532adcec7987a
sam <champ223slol@gmail.com>
2025-09-07 23:13:27 -0500

    added1

--- hello_oop/src/hello_oop/Main.java
+++ hello_oop/src/hello_oop/Main.java
@@ -17,5 +17,8 @@ public class Main {
 
         Greeter drew = new Greeter("Drew Struensee", "Hello word");
         System.out.println(drew.format());
+        
+        Greeter saim = new Greeter("Saim Khan", "Hello, OOP!");
+        System.out.println(saim.format()); 
 	}
 }
