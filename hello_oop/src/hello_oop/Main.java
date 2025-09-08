f6cc1f8b5bf6c9946f206bb07a407448d8a0dc57
MarcusG20 <91750226+MarcusG20@users.noreply.github.com>
2025-09-08 10:41:49 -0500

    Update Main.java

--- hello_oop/src/hello_oop/Main.java
+++ hello_oop/src/hello_oop/Main.java
@@ -17,5 +17,8 @@ public class Main {
 
         Greeter drew = new Greeter("Drew Struensee", "Hello word");
         System.out.println(drew.format());
+
+		Greeter marcus = new Greeter("Marcus Guerrero","Hello World");
+        System.out.println(marcus.format());
 	}
 }
